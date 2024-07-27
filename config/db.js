0170957468124b6591df067578dd430f5485576f
Nandhu-45 <nandasaipavan.4945@gmail.com>
2024-07-27 16:47:07 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,10 +2,19 @@
 const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
+  host: '172.20.10.2',
+  //port: 3306,
+  user: 'root',
+  password: 'newpassword',
+  database: 'payroll',
+
+  /*
   host: 'localhost',
   user: 'root',
   password: 'newpassword',
   database: 'payroll',
+*/
+  
 });
 
 module.exports = db;
